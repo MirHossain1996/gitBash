fb4441a1f11a0a54bd713202da966cccb2715e81
MirHossain <mdmirhossain114@gmail.com>
2023-01-24 14:59:38 +0530

    commit1

---
+++ src/test/java/CMDGitBash/Dummy.java
@@ -0,0 +1,14 @@
+package CMDGitBash;
+
+import org.testng.annotations.Test;
+
+public class Dummy {
+		@Test()
+		public void firstMessage()
+		{
+			System.out.println("---GitbaseTest---");
+			
+		}
+	}
+
+
